a250ef6f119d00434216c35bd62a70a3e29f9fff
Alexander <kozzzloff@list.ru>
2022-10-27 18:25:35 +0400

    Added training and export

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # MobileNet v2 PyTroch on Food101
 Training of MobileNet v2 from Torchvision on Food101 dataset
+
+## Run training:
+```
+python train.py --epochs 30 -j 6 -b 256 --lr 0.001 --pretrained --wd 10e-5 dataset
+```
+
+## Export to ONNX:
+```
+python export.py model_best.pth.tar
+```
---
+++ export.py
@@ -0,0 +1,38 @@
+import sys
+import os
+
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+FOOD101_CLASSES = 101
+
+def fix_names(state_dict):
+    state_dict = {key.replace('module.', ''): value for (key, value) in state_dict.items()}
+    return state_dict
+
+model = models.mobilenet_v2()    
+
+num_ftrs = model.classifier[1].in_features
+model.classifier[1] = nn.Linear(num_ftrs, FOOD101_CLASSES)
+
+if len(sys.argv) > 1:
+    checkpoint_path = sys.argv[1]
+
+    if os.path.isfile(checkpoint_path):
+        print("=> loading checkpoint '{}'".format(checkpoint_path))
+        
+
+        checkpoint = torch.load(checkpoint_path)
+        weights = fix_names(checkpoint['state_dict'])
+        model.load_state_dict(weights)
+
+        print("=> loaded checkpoint '{}' (epoch {})"
+                .format(checkpoint_path, checkpoint['epoch']))
+
+dummy_input = torch.randn(1, 3, 224, 224)
+
+input_names = ["input"] 
+output_names = ["output1"]
+
+torch.onnx.export(model, dummy_input, "mobilenet_v2_food101.onnx", verbose=True, input_names=input_names, output_names=output_names)
