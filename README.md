3d3e160dc41009a41e423f2eab40d9168d7414ff
Alexander <kozzzloff@list.ru>
2022-10-28 15:53:31 +0400

    Added quantization script from the experimental branch of NNCF

--- README.md
+++ README.md
@@ -6,8 +6,13 @@ Training of MobileNet v2 from Torchvision on Food101 dataset
 python train.py --epochs 30 -j 6 -b 256 --lr 0.001 --pretrained --wd 10e-5 dataset
 ```
 
-## Pretrained model:
+## Use pretrained model:
 You can download pretrained model from [here](https://huggingface.co/AlexKoff88/mobilenet_v2_food101). Top-1 accuracy is 76.3%.
+The following script can also automatically downdload the checkpoint and dataset and validate the model:
+```
+python validate_checkpoint.py
+```
+
 
 ## Export to ONNX:
 ```
---
+++ quantize.py
@@ -0,0 +1,80 @@
+import sys
+import os
+
+import numpy as np
+
+import torch
+import nncf
+import torch.nn as nn
+import torchvision.models as models
+
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from sklearn.metrics import accuracy_score
+
+from tqdm import tqdm
+
+FOOD101_CLASSES = 101
+DATASET_PATH = 'dataset'
+CHECKPOINT_URL = 'https://huggingface.co/AlexKoff88/mobilenet_v2_food101/resolve/main/pytorch_model.bin'
+
+def fix_names(state_dict):
+    state_dict = {key.replace('module.', ''): value for (key, value) in state_dict.items()}
+    return state_dict
+
+def load_checkpoint(model):  
+    checkpoint = torch.hub.load_state_dict_from_url(CHECKPOINT_URL, progress=False)
+    weights = fix_names(checkpoint['state_dict'])
+    model.load_state_dict(weights)
+    return model
+
+def validate(model, val_loader):
+    predictions = []
+    references = []
+    
+    with torch.no_grad():
+        for images, target in tqdm(val_loader):
+            output = model(images)
+            predictions.append(np.argmax(output, axis=1))
+            references.append(target)
+
+    predictions = np.concatenate(predictions, axis=0)
+    references = np.concatenate(references, axis=0)  
+    return accuracy_score(predictions, references)
+
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                    std=[0.229, 0.224, 0.225])
+val_dataset = datasets.Food101(
+    root=DATASET_PATH,
+    split = 'test', 
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        normalize,
+    ]),
+    download = True
+)
+val_loader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=128, num_workers=4, shuffle=False)
+
+model = models.mobilenet_v2(num_classes=FOOD101_CLASSES) 
+model.eval()
+model = load_checkpoint(model)
+
+def transform_fn(data_item):
+    images, _ = data_item
+    return images
+
+# Wrap framework-specific data source into the `nncf.Dataset` object.
+calibration_dataset = nncf.Dataset(val_loader, transform_fn)
+
+# Quantization of the PyTorch model.
+quantized_model = nncf.quantize(model, calibration_dataset)
+
+fp32_top1 = validate(model, val_loader)
+
+int8_top1 = validate(quantized_model, val_loader)
+
+print(f'FP32: accuracy @ top1: {fp32_top1}')
+print(f'INT8: accuracy @ top1: {int8_top1}')
